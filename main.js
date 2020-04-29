9506ff40a1647e73d19e78def4debd9eb167cbc0
Obasi Uche <obasiuche62@gmail.com>
2020-04-29 20:15:49 +0100

    added styling to the page

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const documentElement = document.querySelector('html');
+const documentBody = document.querySelector('body');
 const contentElement = document.querySelector('.main-content');
 const sidebarElement = document.querySelector('.sidebar');
 const sidebarIsOpen = () =>
@@ -7,11 +8,13 @@ const sidebarIsOpen = () =>
 const openSidebar = () => {
   /* How you trigger the change is up to you */
   documentElement.classList.add('sidebar-is-open');
+  documentBody.classList.add('blur');
   document.getElementById('myImage').src = 'images/exit.svg';
 };
 
 const closeSidebar = () => {
   documentElement.classList.remove('sidebar-is-open');
+  documentBody.classList.remove('blur');
   document.getElementById('myImage').src = 'images/menu.svg';
 };
 
